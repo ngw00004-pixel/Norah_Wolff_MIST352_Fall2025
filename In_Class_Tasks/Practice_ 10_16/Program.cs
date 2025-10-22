2afbca32c31832d0432d55d91333650f28ee5d45
Norah WOLFF <ngw00004@mix.wvu.edu>
2025-10-22 16:54:57 -0400

    created hW2 folder

---
+++ In_Class_Tasks/Practice_ 10_16/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice__10_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
